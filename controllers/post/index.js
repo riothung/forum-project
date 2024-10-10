0be4d8bdce90aa4d3a7c5769c3e3f893cacd31c3
reymooy27 <gdrrey@gmail.com>
2024-10-10 22:09:50 +0800

    feat: post ->CRUD functions

--- controllers/post/index.js
+++ controllers/post/index.js
@@ -1,36 +1,201 @@
 // controller post
 
+const prisma = require("../../db")
+
+// get post data by id
 const getPostById = async (req, res) => {
   try {
-    console.log(req.params.id)
-    return res.json({
-      data: "user post",
+    const postId = parseInt(req.params.id)
+
+    if (!postId) {
+      return res.status(400).json({
+        message: "Id is not specified",
+        success: false
+      })
+    }
+
+    const posts = await prisma.post.findUnique(
+      {
+        where: {
+          id: postId
+        }
+      })
+    if (!posts) {
+      return res.status(404).json({
+        message: "Post not found",
+        success: false
+      })
+    }
+
+    return res.status(200).json({
+      data: posts,
       success: true
-    }).status(200)
+    })
+
   } catch (error) {
-    return res.json({
-      message: "cannot get user data",
+    return res.status(500).json({
+      message: "Cannot get post",
       success: false
-    }).status(500)
+    })
   }
 }
 
-const createPost = async (req, res) => {
+// get all post data by user
+const getUserPosts = async (req, res) => {
   try {
-    console.log(req.params.id)
-    return res.json({
-      data: "user post",
+    const posts = await prisma.post.findMany({
+      where: {
+        userId: 1 //change id with user id later
+      },
+      take: 10
+    })
+    return res.status(200).json({
+      data: posts,
       success: true
-    }).status(200)
+    })
   } catch (error) {
-    return res.json({
-      message: "cannot get user data",
+    return res.status(500).json({
+      message: "Cannot get posts data",
       success: false
-    }).status(500)
+    })
   }
 }
+
+// create post
+//TODO: need to login
+const createPost = async (req, res) => {
+  //need to sanitize the input
+  //maybe using zod later
+  const { title } = req.body
+
+  if (!title) {
+    return res.status(400).json({
+      message: "Title is required",
+      success: false
+    })
+  }
+
+  try {
+
+    const createdPost = await prisma.post.create({
+      data: {
+        title: title,
+        userId: 1
+      }
+    })
+
+    return res.status(200).json({
+      data: createdPost,
+      message: "Succesfully create post",
+      success: true
+    })
+
+  } catch (error) {
+    console.log(error)
+
+    return res.status(500).json({
+      message: "Cannot create post",
+      success: false
+    })
+
+  }
+}
+
+// delete post
+const deletePost = async (req, res) => {
+
+  const postId = parseInt(req.params.id)
+
+  if (!postId) {
+    return res.status(400).json({
+      message: "ID not specified",
+      success: false
+    })
+  }
+
+  try {
+
+    const deletedPost = await prisma.post.delete({
+      where: {
+        id: postId,
+        userId: 1 //later change to user id
+      }
+    })
+
+    return res.status(200).json({
+      data: deletedPost,
+      message: "Succesfully delete post",
+      success: true
+    })
+
+  } catch (error) {
+    console.log(error)
+
+    return res.status(500).json({
+      message: "Cannot delete post",
+      success: false
+    })
+
+  }
+}
+
+// update/edit post
+const updatePost = async (req, res) => {
+  //TODO: need to sanitize the input
+  //maybe using zod later
+  const postId = parseInt(req.params.id)
+
+  const { title } = req.body
+
+  if (!postId) {
+    return res.status(400).json({
+      message: "ID not specified",
+      success: false
+    })
+  }
+
+  if (!title) {
+    return res.status(400).json({
+      message: "Title is required",
+      success: false
+    })
+  }
+
+  try {
+
+    const updatedPost = await prisma.post.update({
+      data: {
+        title: title,
+      },
+      where: {
+        id: postId,
+        userId: 1 //change to user id later
+      }
+    })
+
+    return res.status(200).json({
+      data: updatedPost,
+      message: "Succesfully update post",
+      success: true
+    })
+
+  } catch (error) {
+    console.log(error)
+
+    return res.status(500).json({
+      message: "Cannot update post",
+      success: false
+    })
+
+  }
+
+}
+
 
 module.exports = {
   getPostById,
-  createPost
+  createPost,
+  getUserPosts,
+  deletePost,
+  updatePost
 }
--- routes/post/index.js
+++ routes/post/index.js
@@ -1,11 +1,15 @@
-const { getPostById, createPost } = require("../../controllers/post");
+const { getPostById, createPost, getUserPosts, deletePost, updatePost } = require("../../controllers/post");
 
 const router = require("express").Router();
 
 // routes post
-
+router.get('/get-user-posts', getUserPosts)
 router.get('/:id', getPostById)
+
+//TODO: implement the protected route later
 router.post('/create', createPost)
+router.delete('/delete/:id', deletePost)
+router.put('/update/:id', updatePost)
 
 module.exports = {
   router
