54ebd5a4e053a6f9f90ac095e78f46ebdbc8d1a5
reymooy27 <gdrrey@gmail.com>
2024-10-10 12:00:07 +0800

    feat: adding dotenv config

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv")
 
 const router = require("./routes/index");
 
 const app = express();
 
 const PORT = 3000
+
+dotenv.config()
 
 app.use(
   cors({
